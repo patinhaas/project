f7dac0cf1349255c6c9106b1ae51c7f1592994a9
YASMINCOS <costayasmin367@gmail.com>
2024-06-06 09:44:22 -0300

    imgs

--- front-end/patinhas/src/components/Header.js
+++ front-end/patinhas/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.png'; 
+import logo from '../imgs/logo.png'; 
 
 function Header() {
   return (
--- front-end/patinhas/src/components/HeaderUser.js
+++ front-end/patinhas/src/components/HeaderUser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo.png'; 
+import logo from '../imgs/logo.png'; 
 
 const HeaderUser = ({ handleLogout }) => {
   return (
